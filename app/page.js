ead966746554410da1ce091c20bae88f20f7a69f
Goyo Cancio <gcancio99@gmail.com>
2024-12-10 18:25:21 +0100

    Enhance layout and add pricing and FAQ sections

--- app/page.js
+++ app/page.js
@@ -1,23 +1,34 @@
 import ButtonLogin from "@/components/ButtonLogin";
+import FAQListItem from "@/components/FAQListItem";
 export default function Home() {
   const isLoggedIn = true;
   const name = "Goyo";
-
+  const pricingItemList = [
+    "Collect customer feedback",
+    "Unlimited boards",
+    "Admin dashboard",
+    "24/7 support",
+  ];
   return (
     <main>
+      {/* HEADER */}
       <section className="bg-base-200">
         <div className="flex justify-between items-center px-8 py-2 max-w-3xl mx-auto">
           <div className="font-bold">CodeFastSaas logo</div>
           <div className="space-x-4 max-md:hidden">
-            <a className="link link-hover">Pricing</a>
-            <a className="link link-hover">FAQ</a>
+            <a className="link link-hover" href="#pricing">
+              Pricing
+            </a>
+            <a className="link link-hover" href="#faq">
+              FAQ
+            </a>
           </div>
           <div>
             <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
           </div>
         </div>
       </section>
-
+      {/* HERO */}
       <section className="text-center py-32 px-8 max-w-3xl mx-auto">
         <h1 className="text-xl sm:text-2xl md:text-4xl lg:text-6xl font-bold mb-6">
           Collect customer feedback to build better products
@@ -27,6 +38,91 @@ export default function Home() {
           products that your customers actually will love
         </div>
         <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+      </section>
+      {/* PRICING */}
+      <section className="bg-base-200" id="pricing">
+        <div className="py-32 px-8 max-w-3xl mx-auto">
+          <p className="text-sm text-center uppercase font-medium text-primary">
+            Pricing
+          </p>
+          <h2 className="text-3xl lg:text-4xl font-extrabold mb-12 text-center text-">
+            A pricing that adapts your needs
+          </h2>
+
+          <div className="p-8 bg-base-100 max-w-96 rounded-3xl mx-auto space-y-5">
+            <div className="flex gap-2 items-baseline">
+              <div className="text-4xl font-black">$19</div>
+              <div className="uppercase text-sm font-medium opacity-60">
+                /month
+              </div>
+            </div>
+
+            <ul className="space-y-2">
+              {pricingItemList.map((priceItem) => {
+                return (
+                  <li className="flex items-center gap-2" key={priceItem}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6 text-green-400 w-4 h-4 "
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m4.5 12.75 6 6 9-13.5"
+                      />
+                    </svg>
+                    {priceItem}
+                  </li>
+                );
+              })}
+            </ul>
+            <div>
+              <ButtonLogin
+                isLoggedIn={isLoggedIn}
+                name={name}
+                extraStyle={"w-full"}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* FAQ */}
+      <section className="bg-base-200" id="faq">
+        <div className="py-32 px-8 max-w-3xl mx-auto">
+          <p className="text-sm text-center uppercase font-medium text-primary">
+            FAQ
+          </p>
+          <h2 className="text-3xl lg:text-4xl font-extrabold mb-12 text-center text-">
+            Frequently Asked Questions
+          </h2>
+
+          <ul className="max-w-lg mx-auto">
+            {[
+              {
+                question: "What is the pricing?",
+                answer: "It is $19/month",
+              },
+              {
+                question: "Can I cancel anytime?",
+                answer: "Yes, you can cancel anytime.",
+              },
+              {
+                question: "Is there a free trial?",
+                answer: "Yes, we offer a 7-day free trial.",
+              },
+              {
+                question: "Do you offer support?",
+                answer: "Yes, we offer 24/7 customer support.",
+              },
+            ].map((qa) => (
+              <FAQListItem key={qa.question} qa={qa} />
+            ))}
+          </ul>
+        </div>
       </section>
     </main>
   );
